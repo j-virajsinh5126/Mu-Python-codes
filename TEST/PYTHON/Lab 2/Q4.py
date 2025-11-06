1d4ecc112fed3d3714f48f2b7a178739367e3639
Virajsinh Jadeja <123184388+j-virajsinh5126@users.noreply.github.com>
2025-11-06 16:08:59 +0530

    Delete TEST directory

--- TEST/PYTHON/Lab 2/Q4.py
+++
@@ -1,6 +0,0 @@
-x = 1
-y= 2
-z = (x+y)*(x-y)
-print("The value of x is:",x)
-print("The value of y is:",y)
-print("The solution od equation is",z)
--- TEST/PYTHON/Lab 2/Q6.py
+++
@@ -1,3 +0,0 @@
-c = float(input("Enter the degree into Celsius:"))
-F = (c * (1.8)) + 32
-print("The degree in Fahrenhit is: ",F)
